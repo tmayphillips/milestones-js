c9863de01da0014808ef4334a7bfdbc61edb79f8
TIFFANY MAY-PHILLIPS <tiffanymay-phillips@TIFFANYs-MacBook-Pro-2.local>
2021-11-02 17:25:22 -0500

    added code completed so far

---
+++ loopingWithObjects.js
@@ -0,0 +1,16 @@
+// Declare an object coolObject containing at least 5 key/value pairs and..
+let coolObject = {
+    name: 'Joe',
+    name: 'Lisa',
+    name: 'Sally',
+    name: 'Marcus',
+    name: 'Abe'
+}
+
+//  Print out every key and value in coolObject
+
+
+//  Without changing coolObject, use sort() to...
+//  Print every key and value in coolObject in alphabetical order (A to Z)
+//  Print every key and value in coolObject in reverse alphabetical order (Z to A)
+//  Iterate over the daysInMonth object from the Objects section, printing out the name of every month that has 31 days.
---
+++ numbers.js
@@ -0,0 +1,50 @@
+//Assign different numbers to two variables named num1 and num2
+let num1 = 20;
+let num2 = 4;
+
+console.log('The value of num1 is:', num1)
+console.log('The value of num2 is:', num2)
+
+//Use the following operations on num1 and num2 and explain what they represent:
+console.log('The sum of num1 and num2 is:', num1 + num2);
+console.log('The difference between num1 and num2 is:', num1 - num2);
+console.log('The product of num1 and num2 is:', num1 * num2);
+console.log('The quotient of num1 and num2 is:', num1 / num2);
+console.log('The remainder of num1 divided by num2 is:', num1 % num2);
+console.log('The exponential of num1 and num2 is:', num1 ** num2);
+console.log('The quotient whole number of num1 and num2 is:', Math.floor(num1 / num2));
+
+//Assign a number to a variable named blah
+let blah = 11541;
+console.log('The value of blah is', blah);
+
+//Get the right-most digit in blah using the % operator
+console.log('The right-most digit of blah is: ', blah % 10);
+
+//Determine whether blah is even or odd
+if (blah % 2 === 0) {
+    console.log('The number blah is even,');
+} else {
+    console.log('The number blah is odd.')
+}
+
+//Increment the value of blah
+k = 5;
+blah++;
+console.log('blah + 1 = ', blah);
+blah = blah + 2;
+console.log('blah + 2 = ', blah);
+blah = blah + 10;
+console.log('blah + 10 = ', blah);
+blah = blah + k;
+console.log('blah + k = ', blah);
+
+//Decrement the value of blah
+blah--;
+console.log('blah - 1 = ', blah);
+blah = blah - 2;
+console.log('blah - 2 = ', blah);
+blah = blah - 10;
+console.log('blah - 10 = ', blah);
+blah - k;
+console.log('blah - k = ', blah);
